644f7a66fc4d183fdaa4d493c34dda5de1d5c8a0
Konrad <fprognoz@gmail.com>
2023-04-17 16:46:55 +0300

    feat: add stack structure

--- src/stack.js
+++ src/stack.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Implement the Stack with a given interface via array.
@@ -13,22 +13,22 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class Stack {
-  push(/* element */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  #elements = [];
+  push(element) {
+    this.#elements[this.#elements.length] = element;
   }
 
   pop() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    const last = this.#elements[this.#elements.length - 1];
+    this.#elements = this.#elements.slice(0, -1);
+    return last;
   }
 
   peek() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.#elements[this.#elements.length - 1];
   }
 }
 
 module.exports = {
-  Stack
+  Stack,
 };
