f35c87a1edf68bc8f935916a3979ce2b5b273a66
Konrad <fprognoz@gmail.com>
2023-04-17 18:22:48 +0300

    feat: add queue structure

--- src/queue.js
+++ src/queue.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 // const { ListNode } = require('../extensions/list-node.js');
 
@@ -14,23 +14,25 @@ const { NotImplementedError } = require('../extensions/index.js');
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
 class Queue {
+  #elements = [];
 
   getUnderlyingList() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    let list = null;
+    for (let i = this.#elements.length - 1; i >= 0; i--) {
+      list = { value: this.#elements[i], next: list };
+    }
+    return list;
   }
 
-  enqueue(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  enqueue(value) {
+    this.#elements.push(value);
   }
 
   dequeue() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.#elements.shift();
   }
 }
 
 module.exports = {
-  Queue
+  Queue,
 };
